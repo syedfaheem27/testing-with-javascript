dd4778a63febf49e7e6c28cc852c381da8f7872a
syedfaheem27 <syedfaheem2719@gmail.com>
2024-02-05 13:33:00 +0530

    Add an integration test for cleanNumbers

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,31 +1,54 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from "./numbers";
+import { it, expect, describe } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it("converts a numeiric string to a value of type number", () => {
-  //Arrange
-  const num = "3";
+describe("transformToNumber()", () => {
+  it("converts a numeiric string to a value of type number", () => {
+    //Arrange
+    const num = "3";
 
-  //Act
-  const result = transformToNumber(num);
+    //Act
+    const result = transformToNumber(num);
 
-  //Assert
-  expect(result).toBeTypeOf("number");
-  expect(result).toBe(+num);
+    //Assert
+    expect(result).toBeTypeOf("number");
+    expect(result).toBe(+num);
+  });
+
+  it("returns NaN for an argument that cannot be converted to a numeric value", () => {
+    const num1 = "invalid";
+    const num2 = {};
+
+    const result1 = transformToNumber(num1);
+    const result2 = transformToNumber(num2);
+
+    expect(result1).toBeNaN();
+    expect(result2).toBeNaN();
+  });
+
+  it("throws an Error if no argument is passed", () => {
+    const resultFn = () => transformToNumber();
+
+    expect(resultFn).toThrow(/No argument passed/);
+  });
 });
 
-it("returns NaN for an argument that cannot be converted to a numeric value", () => {
-  const num1 = "invalid";
-  const num2 = {};
+describe("cleanNumbers()", () => {
+  it("should return an array of numbers when provided with an array of numeric strings", () => {
+    const inputValues = ["1", "2"];
 
-  const result1 = transformToNumber(num1);
-  const result2 = transformToNumber(num2);
+    const result = cleanNumbers(inputValues);
 
-  expect(result1).toBeNaN();
-  expect(result2).toBeNaN();
-});
+    expect(result[0]).toBeTypeOf("number");
+  });
 
-it("throws an Error if no argument is passed", () => {
-  const resultFn = () => transformToNumber();
+  it("should throw an Error when there is atleast a single invalid element in the input array", () => {
+    const inputValues1 = ["", "1"];
+    const inputValues2 = ["invalid", 2];
 
-  expect(resultFn).toThrow(/No argument passed/);
+    const cleanFn1 = () => cleanNumbers(inputValues1);
+    const cleanFn2 = () => cleanNumbers(inputValues2);
+
+    expect(cleanFn1).toThrow();
+    expect(cleanFn2).toThrow();
+  });
 });
